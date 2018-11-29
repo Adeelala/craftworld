b745edb8aa28b169f4e978f3e7bfa2e155d039e0
Qub1 <qub1995@gmail.com>
2018-11-29 16:36:45 +0100

    Add startup text

--- Projects/Client/Source/Main.cpp
+++ Projects/Client/Source/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello World!";
+	std::cout << "Starting client...";
 
 	return 0;
 }
--- Projects/Server/Source/Main.cpp
+++ Projects/Server/Source/Main.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
+
 #include "World.hpp"
 
 int main(int argc, char* argv[]) {
+	std::cout << "Starting server...";
+
 	CraftWorld::World world;
 
 	return 0;
