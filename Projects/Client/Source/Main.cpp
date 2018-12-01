5fa97dc018f18a8888ebe9321448e69891828060
Qub1 <qub1995@gmail.com>
2018-12-01 17:47:23 +0100

    Set up client

--- Projects/Client/Source/Main.cpp
+++ Projects/Client/Source/Main.cpp
@@ -7,55 +7,36 @@ using boost::asio::ip::tcp;
 int main(int argc, char* argv[]) {
 	std::cout << "Starting client...";
 
-	try {
-		// Any program that uses asio need to have at least one io_service object
-		boost::asio::io_service ioService;
+	// Initialize Boost Asio context
+	boost::asio::io_context ioContext;
 
-		// Convert the server name that was specified as a parameter to the application, to a TCP endpoint.
-		// To do this, we use an ip::tcp::resolver object.
-		tcp::resolver resolver(ioService);
+	// Resolve server endpoint
+	tcp::resolver::results_type endpoints = tcp::resolver(ioContext).resolve("localhost", "8000");
 
-		// A resolver takes a query object and turns it into a list of endpoints.
-		// We construct a query using the name of the server, specified in argv[1],
-		// and the name of the service, in this case "daytime".
-		tcp::resolver::query query(argv[1], "daytime");
+	// Create new socket
+	tcp::socket socket(ioContext);
 
-		// The list of endpoints is returned using an iterator of type ip::tcp::resolver::iterator.
-		// A default constructed ip::tcp::resolver::iterator object can be used as an end iterator.
-		tcp::resolver::iterator endpointIterator = resolver.resolve(query);
+	// Establish a connection
+	boost::asio::connect(socket, endpoints);
 
-		// Now we create and connect the socket.
-		// The list of endpoints obtained above may contain both IPv4 and IPv6 endpoints,
-		// so we need to try each of them until we find one that works.
-		// This keeps the client program independent of a specific IP version.
-		// The boost::asio::connect() function does this for us automatically.
-		tcp::socket socket(ioService);
-		boost::asio::connect(socket, endpointIterator);
+	// Keep looping to receive data
+	while(true) {
+		// Receive data
+		std::array<char, 128> buffer;
+		boost::system::error_code errorCode;
+		size_t length = socket.read_some(boost::asio::buffer(buffer), errorCode);
 
-		// The connection is open. All we need to do now is read the response from the daytime service.
-		for(;;) {
-			// We use a boost::array to hold the received data.
-			std::array<char, 1024> buf;
-			boost::system::error_code error;
-
-			// The boost::asio::buffer() function automatically determines
-			// the size of the array to help prevent buffer overruns.
-			size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-			// When the server closes the connection,
-			// the ip::tcp::socket::read_some() function will exit with the boost::asio::error::eof error,
-			// which is how we know to exit the loop.
-			if(error == boost::asio::error::eof) {
-				break; // Connection closed cleanly by peer.
-			} else if(error) {
-				throw boost::system::system_error(error);
-			} // Some other error.
-
-			std::cout.write(buf.data(), len);
+		// Check if data was received
+		if(errorCode == boost::asio::error::eof) {
+			// The connection was closed, so exit
+			break;
+		} else if(errorCode) {
+			// An error occurred
+			throw boost::system::system_error(errorCode);
 		}
-	} catch(std::exception& e) {
-		// handle any exceptions that may have been thrown.
-		std::cerr << e.what() << std::endl;
+
+		// If we're here, print the data
+		std::cout.write(buffer.data(), length);
 	}
 
 	return 0;
--- Projects/Server/Source/Main.cpp
+++ Projects/Server/Source/Main.cpp
@@ -12,16 +12,16 @@ int main(int argc, char* argv[]) {
 
 	CraftWorld::World world;
 
+	boost::asio::io_service ioService;
+
 	try {
-		boost::asio::io_service io_service;
-		tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 8000));
-		tcp::socket socket(io_service);
+		tcp::acceptor acceptor(ioService, tcp::endpoint(tcp::v4(), 8000));
+		tcp::socket socket(ioService);
 		acceptor.accept(socket);
 		std::string message = "Hello world!";
 		boost::system::error_code ignored_error;
 		boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
-	}
-	catch(std::exception& e) {
+	} catch(std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
 
