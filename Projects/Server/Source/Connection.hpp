a7f70dc86db370ec8e2d184d7d09b437cba5553f
Qub1 <qub1995@gmail.com>
2018-12-09 13:37:08 +0100

    Refactor Server classes

--- Projects/Server/Source/Connection.hpp
+++ Projects/Server/Source/Connection.hpp
@@ -1,31 +1,30 @@
 #pragma once
 
+#include <string>
+#include <memory>
 #include <boost/asio.hpp>
 #include <boost/enable_shared_from_this.hpp>
 
 using boost::asio::ip::tcp;
 
 namespace CraftWorld {
+	class Server;
+
 	class Connection :
 		public boost::enable_shared_from_this<Connection> {
-		public:
-			typedef boost::shared_ptr<Connection> pointer;
-
-			static pointer create(boost::asio::io_context& io_context);
-
-			tcp::socket& socket();
-
-			// Handle the client (currently just sends "Hello world!")
-			void start();
-
-		private:
-			Connection(boost::asio::io_context& io_context);
-
-			// Handles the rest of the client connection
-			void handle_write(const boost::system::error_code& error, size_t bytes_transferred);
+			friend Server;
 
 			tcp::socket socket_;
 
-			std::string message_;
+			// Handles the rest of the client connection
+			void handleWrite(const boost::system::error_code& error, size_t bytes_transferred);
+
+			void send(const std::string& message) const;
+
+		public:
+			Connection(Server& server);
+
+			// Handle the client (currently just sends "Hello world!")
+			void start();
 	};
 }
--- Projects/Server/Source/Main.cpp
+++ Projects/Server/Source/Main.cpp
@@ -11,11 +11,10 @@ int main(int argc, char* argv[]) {
 
 	try {
 		// Create the server object that is going to accept connections
-		boost::asio::io_context io_context;
-		Server server(io_context);
+		Server server;
 
 		// Run the IO context so that it will perform asynchronous operations
-		io_context.run();
+		server.run();
 	} catch(std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
