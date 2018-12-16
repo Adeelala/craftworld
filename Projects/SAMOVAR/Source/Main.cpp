fe9a7671b52d9802551330ec03e00711caab45df
Revle <Kwanwin13@gmail.com>
2018-12-16 14:30:16 +0100

    fixed player coordinates error

--- Projects/SAMOVAR/Source/Main.cpp
+++ Projects/SAMOVAR/Source/Main.cpp
@@ -43,12 +43,12 @@ int_vec_t moveNorth(int_vec_t map, player_coord player){
 	/*Now that we have all the necessary data, we can start moving the character*/
 
 	/*Check if the player is trying to move out of bounds into a different map*/
-	if(player_z + 1 > map_vector_dimension_size - 1){
+	if(player.z + 1 > map_vector_dimension_size - 1){
 		/*!!Need to discuss how to swap to another map!!*/
 	}
 	else{
 		/*Check if the players upper body will stay in the map*/
-		if(player_y + 1 > map_vector_dimension_size - 1){
+		if(player.y + 1 > map_vector_dimension_size - 1){
 			/*If the player cannot be moved just return the map and player as is*/
 			return map, player;
 
@@ -61,7 +61,7 @@ int_vec_t moveNorth(int_vec_t map, player_coord player){
 		is a dirk block*/
 		//if(map[(map_y_loc_end + 1) + player_z] == 0 && map[(map_y_loc_end + 1) + player_z] == 0){
 
-		else if(map[player.x][player.y][player.z + 1] == 0 && map[player_x][player.y + 1][player.z + 1]){
+		else if(map[player.x][player.y][player.z + 1] == 0 && map[player.x][player.y + 1][player.z + 1]){
 			/*Old position gets filled with air*/
 			map[player.x][player.y][player.z] = 1;
 			map[player.x][player.y][player.z] = 1;
@@ -70,8 +70,7 @@ int_vec_t moveNorth(int_vec_t map, player_coord player){
 			map[player.x][player.y][player.z + 1] = 2;
 			map[player.x][player.y + 1][player.z + 1] = 2;
 
-			player_x = player.x;
-			player_y = player.y;
+			
 			player_z = player.z + 1;
 
 		}
